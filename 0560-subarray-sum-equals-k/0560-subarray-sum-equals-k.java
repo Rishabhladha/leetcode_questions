f6014e426fdf988ca588510be3720bd0ed0c2ae2
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-06-19 01:20:43 +0530

    Time: 23 ms (93.49%), Space: 48.9 MB (46.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,47 +1,47 @@
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         int sum =0;
-//         int count =0;
-//         for(int i=0;i<nums.length;i++){
-//             sum +=nums[i];
-//             //case1
-//             if(sum==k){
-//                 count++;
-//             }
-//             //case2
-//             int needed = sum-k;
-//             if(map.containsKey(needed)){
-//                 count += map.get(needed);
-//             }
-
-//              map.put(sum, map.getOrDefault(sum,0)+1);
-
-//         }
-//         return count;
-//     }
-// }
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        map.put(0, 1);  // base case
-        
-        int count = 0;
-        int sum = 0;
-        
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-            
-            int needed = sum - k;
-            
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum =0;
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+            sum +=nums[i];
+            //case1
+            if(sum==k){
+                count++;
+            }
+            //case2
+            int needed = sum-k;
             if(map.containsKey(needed)){
                 count += map.get(needed);
             }
-            
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+             map.put(sum, map.getOrDefault(sum,0)+1);
+
         }
-        
         return count;
     }
 }
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         map.put(0, 1);  // base case
+        
+//         int count = 0;
+//         int sum = 0;
+        
+//         for(int i = 0; i < nums.length; i++){
+//             sum += nums[i];
+            
+//             int needed = sum - k;
+            
+//             if(map.containsKey(needed)){
+//                 count += map.get(needed);
+//             }
+            
+//             map.put(sum, map.getOrDefault(sum, 0) + 1);
+//         }
+        
+//         return count;
+//     }
+// }
