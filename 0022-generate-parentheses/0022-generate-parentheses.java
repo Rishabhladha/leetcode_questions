a976e82c27e8f87c371949fde45d74c9e33e5e70
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-06-22 17:38:38 +0530

    Time: 2 ms (71.99%), Space: 45 MB (13.77%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -10,11 +10,9 @@ class Solution {
     public void solve(int n, String curr, ArrayList<String> ans, int count1, int count2){
         
         if(curr.length()==n*2){
-            if(curr.charAt(0)=='(' && curr.charAt(curr.length()-1)==')'){
                 ans.add(curr);
                 return;
-            }
-            return;
+            
         }
         if(count1<n){
         solve(n,curr+"(",ans,count1+1,count2);
