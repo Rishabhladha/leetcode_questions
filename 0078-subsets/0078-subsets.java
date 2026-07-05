cd358cdd9cd7574b59c7d45a491d86d1b2414081
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-07-05 12:59:05 +0530

    Time: 1 ms (87.77%), Space: 44 MB (88.07%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -11,23 +11,23 @@ class Solution {
 
     public void find(int index, int[] nums, List<List<Integer>> ans, List<Integer> curr){
 
-        // if(index==nums.length){
-        //     ans.add(new ArrayList<>(curr));
-        //     return;
-        // }
+        if(index==nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
 
-        // curr.add(nums[index]);
-        // find(index+1,nums,ans,curr);
-        // curr.remove(curr.size()-1);
-        // find(index+1,nums,ans,curr);
+        curr.add(nums[index]);
+        find(index+1,nums,ans,curr);
+        curr.remove(curr.size()-1);
+        find(index+1,nums,ans,curr);
 
-        ans.add(new ArrayList<>(curr));
+    //     ans.add(new ArrayList<>(curr));
 
-    for(int i = index; i < nums.length; i++) {
-        curr.add(nums[i]);
-        find(i + 1, nums, ans, curr);
-        curr.remove(curr.size() - 1);
-    }
+    // for(int i = index; i < nums.length; i++) {
+    //     curr.add(nums[i]);
+    //     find(i + 1, nums, ans, curr);
+    //     curr.remove(curr.size() - 1);
+    // }
 
     }
 }
