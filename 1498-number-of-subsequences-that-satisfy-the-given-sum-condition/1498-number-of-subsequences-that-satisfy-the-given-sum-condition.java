e1ec96b7576fbb81768b9fe99a6f5607aa280923
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-07-05 17:09:58 +0530

    Time: 33 ms (57.82%), Space: 66.7 MB (54.98%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -35,32 +35,45 @@ class Solution {
         return (int) ans;
     }
 }
+
 //this code will not work because time jada lg ra he mod bacha nhi paye ga
 // class Solution {
-//     int mod = 1000000007;
-//     int count=0;
+
+//     int count = 0;
+
 //     public int numSubseq(int[] nums, int target) {
+
+//         Arrays.sort(nums);   // Needed so first = min and last = max
+
 //         List<Integer> ans = new ArrayList<>();
-//        solve(0,nums,taget,ans);
+
+//         solve(0, nums, target, ans);
+
 //         return count;
 //     }
 
-//     public void solve(int index, int[] nums, int target, int ans){
+//     public void solve(int index, int[] nums, int target, List<Integer> ans) {
 
-//             if(index == nums.length){
+//         if (index == nums.length) {
+
+//             if (ans.size() > 0) {
+
 //                 int start = ans.get(0);
-//                 int end = ans.get(ans.size()-1);
-//                 if(start+end==target){
+//                 int end = ans.get(ans.size() - 1);
+
+//                 if (start + end <= target) {
 //                     count++;
-//                     return;
 //                 }
 //             }
-
-//         if(nums[index]<=target){
-//             ans.add(nums[index]);
-//             solve(index+1,nums,target,ans);
-//             ans.remove(ans.size()-1);
+//             return;
 //         }
-//         solve(index+1,nums,target,ans);
+
+//         // Take
+//         ans.add(nums[index]);
+//         solve(index + 1, nums, target, ans);
+//         ans.remove(ans.size() - 1);
+
+//         // Not take
+//         solve(index + 1, nums, target, ans);
 //     }
 // }
