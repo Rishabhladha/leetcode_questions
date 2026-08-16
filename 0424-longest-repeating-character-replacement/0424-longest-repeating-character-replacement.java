f99f65597ac83b05e4872577845bfec477ede309
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-08-16 22:38:43 +0530

    Time: 8 ms (66.37%), Space: 46.5 MB (30.54%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -19,3 +19,30 @@ class Solution {
         return maxLen;
     }
 }
+//brute force approch given below
+
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+        
+//          int maxLen=0;
+
+//         for(int i=0;i<s.length();i++){
+//             int hash[] = new int[26];
+//             int maxFreq =0;
+
+//             for(int j=i;j<s.length();j++){
+
+//                 hash[s.charAt(j)-'A']++;
+//                 maxFreq = Math.max(maxFreq, hash[s.charAt(j)-'A']);
+//                 int window = j-i+1;
+//                 int change = window-maxFreq;
+//                 if(change<=k){
+//                    maxLen = Math.max(maxLen, j-i+1);
+//                 }else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxLen;
+//     }
+// }
