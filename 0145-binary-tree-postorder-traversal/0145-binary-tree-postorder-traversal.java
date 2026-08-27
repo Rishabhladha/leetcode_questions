125b964267196c99997ce00d444b059d619857cd
RISHABH LADHA <152367457+Rishabhladha@users.noreply.github.com>
2026-08-27 14:26:34 +0530

    Time: 0 ms (100%), Space: 43.3 MB (15.24%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,19 +15,49 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        postorder(root, ans);
-        return ans;
-    }
+        List<Integer> result = new ArrayList<>();
 
-    public void postorder(TreeNode root, List<Integer> ans){
-        if(root == null){
-            return;
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        TreeNode current = root;
+        if(root == null) return result;
+        s1.push(current);
+        while(!s1.isEmpty()){
+            current = s1.pop();
+            s2.push(current.val);
+
+            if(current.left !=null){
+                s1.push(current.left);
+            }
+
+            if(current.right != null){
+                s1.push(current.right);
+            }
         }
-
-        postorder(root.left, ans);
-        postorder(root.right, ans);
-
-        ans.add(root.val);
+        while(!s2.isEmpty()){
+            result.add(s2.pop());
+        }
+        return result;
     }
 }
+
+
+ // the recursive approch
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         postorder(root, ans);
+//         return ans;
+//     }
+
+//     public void postorder(TreeNode root, List<Integer> ans){
+//         if(root == null){
+//             return;
+//         }
+
+//         postorder(root.left, ans);
+//         postorder(root.right, ans);
+
+//         ans.add(root.val);
+//     }
+// }
